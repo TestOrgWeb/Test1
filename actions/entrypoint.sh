44f641458561730828d608633f31bdf6c0dcea76
Sayali Gaikawad <gaiksaya@amazon.com>
2020-04-10 16:17:37 -0700

    Docker base image

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -1,19 +1,4 @@
 #!/bin/sh -l
 
 echo "${NAME}"
-
-# Set up .netrc file with GitHub credentials
-cat <<- EOF > .netrc
-    machine github.com
-    login gaiksaya
-    password ${ACCESS_TOKEN}
-    machine api.github.com
-    login gaiksaya
-    password ${ACCESS_TOKEN}
-EOF
-chmod 600 .netrc
-cat .netrc
-
-git init
-git clone git@github.com:TestOrgWeb/infra.git
-echo "git clone worked"
+echo `git branch`
