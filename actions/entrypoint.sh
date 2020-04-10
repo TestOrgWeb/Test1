034f0b25957927cb7aa86218ef9cd67c4ee7ee0f
Sayali Gaikawad <gaiksaya@amazon.com>
2020-04-10 15:40:21 -0700

    git remote

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/sh -l
 
 echo "${NAME}"
-echo "Viewing git remote"
+
+# Set up .netrc file with GitHub credentials
+cat <<- EOF > $HOME/.netrc
+    machine github.com
+    login gaiksaya
+    password 8486954681412a2464f9c6acbc1d6f7fde11c2e4
+    machine api.github.com
+    login gaiksaya
+    password 8486954681412a2464f9c6acbc1d6f7fde11c2e4
+EOF
+chmod 600 $HOME/.netrc
+
+echo "checking git remote"
 git remote -v
-# Making sure we are on top of the branch
-echo "Git checkout branch ${GITHUB_REF##*/}"
-git checkout ${GITHUB_REF##*/}
