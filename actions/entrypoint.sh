fb6b92f457064f8852561d5d54ca8ba30c0ec676
Sayali Gaikawad <gaiksaya@amazon.com>
2020-04-10 15:19:23 -0700

    git remote

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -l
 
 echo "${NAME}"
-echo 1
-echo "$NAME"
-echo 2
-echo "Single quotes:'$NAME'"
-echo 3
-echo "Single with brac: '${NAME}'"
+echo "Viewing git remote"
+git remote -v
+# Making sure we are on top of the branch
+echo "Git checkout branch ${GITHUB_REF##*/}"
+git checkout ${GITHUB_REF##*/}
