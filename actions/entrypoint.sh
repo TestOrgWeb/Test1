1e8f783d29e1b564aa39e574ad6d09bf088a15aa
Sayali Gaikawad <gaiksaya@amazon.com>
2020-04-10 19:08:22 -0700

    Branches name update

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -3,6 +3,8 @@
 echo "${NAME}"
 echo "------------Remote url-----------"
 git remote -v
+echo "------------Clone repo-----------"
+git clone ${NAME}
 echo "-------------branch name-------------------"
 git branch
 echo "-------------Checking out the branch------------"
