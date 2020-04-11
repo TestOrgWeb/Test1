40d9daca20bfb4f4e3de7b4dc703fbf20c75e6cf
Sayali Gaikawad <gaiksaya@amazon.com>
2020-04-10 19:25:47 -0700

    Jpdate

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -3,8 +3,6 @@
 echo "${NAME}"
 echo "------------Remote url-----------"
 git remote -v
-echo "------------Clone repo-----------"
-git clone ${NAME}
 echo "-------------branch name-------------------"
 git branch
 echo "-------------Checking out the branch------------"
