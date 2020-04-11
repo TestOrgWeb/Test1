d1b97a14a5c393d39a1a4c8308bdb2487efd2c44
Sayali Gaikawad <gaiksaya@amazon.com>
2020-04-10 18:55:01 -0700

    checking out branch

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -5,3 +5,5 @@ echo "------------Remote url-----------"
 git remote -v
 echo "-------------branch name-------------------"
 git branch
+echo "-------------Checking out the branch------------"
+git checkout ${{ env.branchName }}
