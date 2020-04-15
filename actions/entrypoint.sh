3cdfc6bc1c63fc6195cafca08ad67be2950e7410
Sayali Gaikawad <gaiksaya@amazon.com>
2020-04-14 17:42:22 -0700

    Main logic added

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -1,13 +1,31 @@
 #!/bin/sh -l
 
-echo "${NAME}"
+echo "Repository name: ${NAME}"
 echo "------------Remote url-----------"
 git remote -v
-echo "-------------branch name-------------------"
-git branch
-echo "-------------Checking out the branch------------"
-git fetch
-git checkout ${BRANCHES}
-git branch
-pwd echo "-------------------"
+pwd 
+echo "---------See the git config----------"
 ls -al
+cat .git/config
+for i in $(git branch -r | grep ${BRANCH} | sed 's/origin\///'); do
+    git fetch
+    git checkout ${i}
+    git branch
+    echo "--------creating autobackport branch for raising PR-------------"
+    git checkout -b autoBackport-${i} origin/${i}
+    echo "----------Cherry picking the commit-------------------"
+    git cherry-pick ${COMMIT_SHA}
+    if [ echo$? -ne 0 ]
+    then echo "Need manual intervention to resolve the conflicts"
+    else
+    echo "--------Push the branch to upstream-------------"
+    git push -f origin autoBackport-${i}
+    echo "-------Creating PR----------------"
+    curl -X POST \
+    https://api.github.com/repos/${NAME}/pulls?access_token=${ACCESS_TOKEN} \
+    -H 'Content-Type: application/json' \
+    -H 'Authorization: Bearer ${ACCESS_TOKEN}' \
+    -d '{ \"title\":\"Backporting PR\", \"body\":\"Backporting the changes to previous version\", \"head\":\"autobackport-${i}\",\"base\":\"${i}\"}'
+    fi
+
+done
