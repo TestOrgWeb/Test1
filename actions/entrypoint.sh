3dc0159b0447bf0eeb7acf6b5efc91acafbe8135
Sayali Gaikawad <gaiksaya@amazon.com>
2020-04-14 19:59:43 -0700

    Main logic test

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -5,39 +5,28 @@ echo "Repository name: ${NAME}"
 echo "------------Branch name-----------"
 git fetch
 echo ${BRANCH} 
-echo "----------All branches---------------"
-echo `git branch -r`
-a=`git branch -r | grep ${BRANCH} | sed 's/origin\///'`
-echo "----------Value of a----------------"
-echo $a
+backportingBranches=`git branch -r | grep ${BRANCH} | sed 's/origin\///'`
 echo "---------For loop-----------"
-for i in $a; 
-do
+for i in $backportingBranches; do
+    echo "Current branch in process: "
     echo ${i}
+    git checkout ${i}
+    git branch
+    echo "--------creating autobackport branch for raising PR-------------"
+    git checkout -b autoBackport-${i} origin/${i}
+    echo "----------Cherry picking the commit-------------------"
+    git cherry-pick ${COMMIT_SHA}
+    if [ `echo $?` -ne 0 ]
+    then echo "Need manual intervention to resolve the conflicts"
+    else
+    echo "--------Push the branch to upstream-------------"
+    git push -f origin autoBackport-${i}
+    echo "-------Creating PR----------------"
+    `curl -X POST \
+    https://api.github.com/repos/${NAME}/pulls?access_token=${ACCESS_TOKEN} \
+    -H 'Content-Type: application/json' \
+    -H 'Authorization: Bearer ${ACCESS_TOKEN}' \
+    -d '{ \"title\":\"Backporting PR\", \"body\":\"Backporting the changes to previous version\", \"head\":\"autobackport-${i}\",\"base\":\"${i}\"}'`
+    fi
+
 done
-
-# for i in $(git branch -r | grep ${BRANCH} | sed 's/origin\///'); do
-#     echo "branch in for loop : "
-#     echo ${i}
-#     git fetch
-#     git checkout ${i}
-
-#     git branch
-#     echo "--------creating autobackport branch for raising PR-------------"
-#     git checkout -b autoBackport-${i} origin/${i}
-#     echo "----------Cherry picking the commit-------------------"
-#     git cherry-pick ${COMMIT_SHA}
-#     if [ echo$? -ne 0 ]
-#     then echo "Need manual intervention to resolve the conflicts"
-#     else
-#     echo "--------Push the branch to upstream-------------"
-#     git push -f origin autoBackport-${i}
-#     echo "-------Creating PR----------------"
-#     curl -X POST \
-#     https://api.github.com/repos/${NAME}/pulls?access_token=${ACCESS_TOKEN} \
-#     -H 'Content-Type: application/json' \
-#     -H 'Authorization: Bearer ${ACCESS_TOKEN}' \
-#     -d '{ \"title\":\"Backporting PR\", \"body\":\"Backporting the changes to previous version\", \"head\":\"autobackport-${i}\",\"base\":\"${i}\"}'
-#     fi
-
-# done
