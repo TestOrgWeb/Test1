5a65fdbf36734d8e1393753f84c613cf49a8a83c
Sayali Gaikawad <gaiksaya@amazon.com>
2020-04-20 17:40:33 -0700

    Json rectified

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -11,27 +11,31 @@ git fetch
 #setting up workflow URL
 workflowUrl=https://github.com/TestOrgWeb/infra/actions/runs/${GITHUB_RUN_ID}
 
-#Branches where changes need to be
+#Branches where changes need to be backported
 echo "Regex specified: ${BRANCH}"
 git branch -r | sed 's/origin\///' >> regex
 backportingBranches=`grep "^  ${BRANCH}" regex`
 rm regex
-echo "------------Branch/es name-----------"
+echo "------------Branch/es where changes are to be backported-----------"
 echo $backportingBranches
 
 echo "----------------Backporting now----------------"
 for i in $backportingBranches; do
     echo "Current branch in process: ${i} "
     git checkout ${i}
-    echo "--------creating autobackport branch for raising PR-------------"
-    git checkout -b autoBackport-${i} origin/${i}
+
+    echo "--------creating a temp autobackport branch for raising PR-------------"
+    dt=`date +"%y%m%dT%H%M"`
+    autobranch=autoBackport-${i}-$dt 
+    git checkout -b $autobranch origin/${i}
+
     echo "----------Cherry picking the commit-------------------"
     git cherry-pick ${COMMIT_SHA}
-    if [ `echo $?` -ne 0 ]
+    if [ `echo $?` -ne 0 ] #failure scenario
     then
     git add .
-    git commit -m "Merge Conflicts here! Need to be resolved"
-    git push origin autoBackport-${i}
+    git commit -m "Merge Conflicts here!"
+    git push origin $autobranch
     echo "BACKPORTING FAILED FOR BRANCH $i!!!!! NEED MANUAL INTERVENTION TO RESOLVE CONFLICTS"
     # Creating PR now
     echo "---------Creating merge conflict PR----------------"
@@ -41,19 +45,19 @@ for i in $backportingBranches; do
     -d "{
          \"title\":\"Backporting PR with conflicts\", 
          \"body\":\"This PR has merge conflicts. Please refer to the files changed tab, resolve and then merge\", 
-         \"head\":\"autoBackport-${i}\",
+         \"head\":\"$autobranch\",
          \"base\":\"${i}\"
          }")
-    pull_url=$(echo "$response" | jq .html_url)
+    pull_url=$(echo "$response" | jq .html_url | sed 's/\"//g')
     # Informing the user via PR comment that it failed
     echo `curl -X POST ${PR_URL} -H 'Content-Type: application/json' \
     -H "Authorization: Bearer ${ACCESS_TOKEN}" \
     -d "{ 
-        \"body\" : \"Backporting attempted at ${COMMENTTIME} failed for branch $i!! Manual intervention required to resolve the conflicts. Workflow URL - $workflowUrl PR- $pull_url\"
+        \"body\" : \"Backporting attempted at ${COMMENTTIME} failed for branch $i!! Manual intervention required to resolve the conflicts. Workflow URL - $workflowUrl New PR URL- $pull_url\"
         }"`
-    else
+    else #success scenario
     echo "--------Push the branch to upstream-------------"
-    git push origin autoBackport-${i}
+    git push origin $autobranch
     # Creating PR now
     echo "---------Creating PR----------------"
     res=$(curl -X POST https://api.github.com/repos/${NAME}/pulls \
@@ -62,17 +66,17 @@ for i in $backportingBranches; do
     -d "{
          \"title\":\"Backporting PR\", 
          \"body\":\"Backporting the changes to previous version\", 
-         \"head\":\"autoBackport-${i}\",
+         \"head\":\"$autobranch\",
          \"base\":\"${i}\"
          }")
-    new_pr=$(echo "$res" | jq.html_url)
+    new_pr=$(echo "$res" | jq .html_url | sed 's/\"//g')
     echo "Backporting successful for branch: $i"
 
     # Informing the user via PR comment that it succeeded
     echo `curl -X POST ${PR_URL} -H 'Content-Type: application/json' \
     -H "Authorization: Bearer ${ACCESS_TOKEN}" \
     -d "{ 
-        \"body\" : \"Backporting attempted at ${COMMENTTIME} successful for branch $i Workflow URL- $workflowUrl PR URL- $new_pr\" 
+        \"body\" : \"Backporting attempted at ${COMMENTTIME} successful for branch $i Workflow URL- $workflowUrl New PR URL- $new_pr\" 
         }"`
 
     fi
